540cc7fd748488229f26fa701a4c7ab066e47ff3
jungyunmok <jungyunmok@gmail.com>
2023-11-17 21:03:11 +0900

    Refactor: 숫자, 중복 체크 메서드 반환타입 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,32 +6,33 @@ import java.util.List;
 public class Game {
     private List<Integer> userNumbers;
 
-    public List<Integer> checkInt(String input) {
+    public int checkInt(String input) {
         int inputNumbers;
         try {
             inputNumbers = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자 이외의 값 입력");
         }
-        userNumbers = new ArrayList<>();
-        while (userNumbers.size() < Constant.MAXRANGE) {
-            userNumbers.add(checkNumber(inputNumbers));
-        }
-        return userNumbers;
+        return inputNumbers;
     }
 
-    public int checkNumber(int inputNumbers) {
-        if(inputNumbers < 0) {
+    public void checkNumber(int inputNumbers) {
+        if (inputNumbers < 0) {
             throw new IllegalArgumentException("[ERROR] 잘못된 숫자 입력");
         }
+        userNumbers = new ArrayList<>();
         int number = 0;
         for (int i = 100; i > 0; i = i / 10) {
             number = (inputNumbers % i * 10) / i;
             if (userNumbers.contains(number) || number == 0) {
                 throw new IllegalArgumentException("[ERROR] 잘못된 숫자 입력");
             }
+            userNumbers.add(number);
         }
-        return number;
+    }
 
+    public String judgement() {
+
+        return "";
     }
 }
